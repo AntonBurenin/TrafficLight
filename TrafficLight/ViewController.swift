e9ee6999aa81386cd8bc286b4321be5a2f9936cc
AntonBu <burkinband@mail.ru>
2020-05-21 01:09:01 +0300

    статический размер

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,12 +9,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var lightStack: UIStackView!
+    
+    private var clickCounter = -1
+    private let lightOff:CGFloat = 0.3
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        for view in lightStack.arrangedSubviews {
+            startLightConfig(view)
+        }
+        startButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func buttonClick(_ sender: UIButton) {
+        if clickCounter == -1 {
+            startButton.setTitle("NEXT", for: .normal)
+            clickCounter = 0
+        }
+        lightStack.arrangedSubviews[clickCounter % 3].alpha = 1
+        lightStack.arrangedSubviews[(clickCounter + 2) % 3].alpha = lightOff
+        clickCounter += 1
+    }
+    
+    private func startLightConfig(_ view: UIView) {
+        view.layer.cornerRadius = view.frame.width  / 2
+        view.alpha = lightOff
+    }
 }
 
